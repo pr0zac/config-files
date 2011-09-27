54c3925c907ee3d28d8a99e3577f655245269db8
Zac Morris <zac@caffeinatedmind.com>
2011-09-27 15:55:44 -0700

    Updating vim to use rbenv instead of rvm and updating vim for new version of yankring

--- bash/env.sh
+++ bash/env.sh
@@ -4,11 +4,10 @@ WORKON_HOME=~/.virtualenvs
 
 mkdir -p $WORKON_HOME
 
-PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH
+PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$HOME/.rbenv/shims:$PATH
 EDITOR=vim
-
 NODE_PATH=/usr/local/lib/node
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
-
 export PATH EDITOR WORKON_HOME NODE_PATH
+
+eval "$(rbenv init -)"
